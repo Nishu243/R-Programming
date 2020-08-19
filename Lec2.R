7060109d19a12cbd5e6f894b15f7d60cc9c1dcb2
Nishu243 <69872663+Nishu243@users.noreply.github.com>
2020-08-19 21:22:15 +0600

    Update Lec2.R

--- Lec2.R
+++ Lec2.R
@@ -1,4 +1,9 @@
- #create a data frame
+###data frame is most used data structure. From data frame function, we can construct a table by using column and row from a given set of data. we can determine the dimention of that table(number of rows and 
+##number of columns), we can calculate mean, median, quartile value of tabular dataset, can print desired column or rows, can know the type of that column, can construct a new data
+## frame by taking desired number of row or columns from first dataframe. Data frame is necessary for different R package and R function.
+
+
+#create a data frame
 df=data.frame(col1=1:4, col2=4:7, col3=7:10)
 #explore data frame
 #print column names
