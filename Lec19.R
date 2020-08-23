0c97f0d94c2eef070c18d63eda733792066f7198
Nishu243 <69872663+Nishu243@users.noreply.github.com>
2020-08-23 23:31:47 +0600

    Add files via upload

---
+++ Lec19.R
@@ -0,0 +1,21 @@
+#If statement
+x=5
+if(x>0) {
+  print("positive number")
+} else {
+  print("negative number")
+}
+
+#add else if
+index=0
+if(x>0) {
+  print("positive number")
+} else if(x<0) {
+  print("negative number")
+} else {
+  print("its zero")
+}
+
+#In dplyr package,there are use of else if.
+
+
