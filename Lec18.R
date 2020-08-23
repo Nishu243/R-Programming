b223c5a864ac209b738ad94d85980be3b5ef8459
Nishu243 <69872663+Nishu243@users.noreply.github.com>
2020-08-23 23:34:28 +0600

    Add files via upload

---
+++ Lec18.R
@@ -0,0 +1,14 @@
+#Data visualization with ggplot2
+#using different geom functions,we can draw different types of plot
+#those info will be found in Jeffron slides
+#
+geom_are()
+#
+geom_density()
+geom_dotplot()
+etc
+#for changing the color of bar of geom_bar plot, we can select
+the color from this website:
+https://colorbrewer2.org/#type=sequential&scheme=BuGn&n=3
+#
+http://www.sthda.com/english/wiki/ggplot2-colors-how-to-change-colors-automatically-and-manually
