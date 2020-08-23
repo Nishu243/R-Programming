d640c895721f54d8e8ec2a615eca3377292f7c59
Nishu243 <69872663+Nishu243@users.noreply.github.com>
2020-08-23 23:35:12 +0600

    Add files via upload

---
+++ Lec20.R
@@ -0,0 +1,31 @@
+#Loop enable a code to execute repeatedly
+#For loop and while loop
+for (index in 1:10)
+{
+  print(index)
+}  
+
+#
+for (index in 1:10)
+{
+  print(c("index=",index))
+}  
+
+#multiply 1 or any number by 1:10
+
+for (index in 1:10)
+{
+  print(1*index)
+}
+#
+for (index in 1:10)
+{
+  print(1*index)
+}
+#concatation
+for (index in 1:10)
+{
+  print(c(3,"*",index,"=",3*index))
+}
+#also check while loop in R
+
