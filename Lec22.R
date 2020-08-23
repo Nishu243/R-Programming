c9fe5c1081a20cef01bd3f2a7320aa94d763c302
Nishu243 <69872663+Nishu243@users.noreply.github.com>
2020-08-23 23:36:40 +0600

    Add files via upload

---
+++ Lec22.R
@@ -0,0 +1,8 @@
+#Hierarchical clustering is an algorithm that groups similar 
+#objects into groups called clusters
+#bottom-up approach and top-down approach
+#it is used for quality control (QC) check, to check swap in data,
+#or to check whether is clustered according to our expectation
+# we can found the status of gene expression from following sites
+# https://www.ebi.ac.uk/gxa/home
+#https://www.ebi.ac.uk/gxa/experiments/E-MTAB-2770
